1749caabd9e0cebe74130b9dd7ced1e8d899ec5d
Neo Ellison <neo@fixitwithcode.com>
2014-11-03 15:00:17 -0500

    add in the test stubs

---
+++ spec/clj/cljs_hit_the_ground_running/server_spec.clj
@@ -0,0 +1,7 @@
+(ns cljs-hit-the-ground-running.server-spec
+  (:require [speclj.core :refer :all]
+            [cljs-hit-the-ground-running.server :refer :all]))
+
+(describe "Web Tests"
+          (it "It has app"
+              (should run)))
---
+++ spec/cljs/cljs_hit_the_ground_running/core_spec.cljs
@@ -0,0 +1,22 @@
+(ns cljs-hit-the-ground-running.core-spec
+  (:require-macros [speclj.core :refer [describe it should=]])
+  (:require [speclj.core :refer :all]
+            [cljs-hit-the-ground-running.core :refer :all]))
+
+(describe "A ClojureScript test"
+  (it "fail. Fix it!"
+    (should= 1 1)))
+
+
+;; Tutorial Tests:
+
+;; (.log js/console (dom/getElement "query"))
+;; Shows the dom element named #query
+
+;; indicates a click
+#_(let [clicks (listen (dom/getElement "search") "click")]
+  (go (while true
+        (.log js/console (<! clicks)))))
+
+;; test the jsonp query
+;; (go (.log js/console (<! (jsonp (query-url "cats")))))
